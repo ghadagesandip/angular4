1315469d0abfcec88112ec7c837245c6c455f510
Sandip Ghadge <sandip.ghadge@wwindia.com>
2017-08-17 18:19:11 +0530

    done lecture 42

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component'
 
 
@@ -15,7 +15,7 @@ import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edi
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShopingListComponent, ShoppingEditComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
