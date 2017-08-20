470cb133bfd466595d2d2a60ac24fbfdc7e9102d
sandip-home <ghadagesandip@gmail.com>
2017-08-20 17:49:27 +0530

    done with recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('Dal Rice', 'Combination of dal and rice', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1074500_11.jpg?itok=IwEifJO_'),
+    new Recipe('Modak', 'Modak used as prasad in Ganesh uthsav', 'https://i.ytimg.com/vi/e_RZSfwlEOE/maxresdefault.jpg')
+  ]
 
   constructor() { }
 
